4f106aa17275c53f8d17baa8bd863da68293a7ee
Alec Hipshear <alec.hipshear@getbraintree.com>
2015-10-16 14:50:57 -0400

    More configuration; meant to include asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
-# require "sprockets/railtie"
+require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -31,5 +31,11 @@ module Doglog
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |generate|
+      generate.helper false
+      generate.javascript_engine false
+      generate.stylesheets false
+    end
   end
 end
