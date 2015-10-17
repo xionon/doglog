850d7c73885958a6b845e6ae2f221d067ec3690e
Alec Hipshear <alechipshear@gmail.com>
2015-10-17 11:02:02 -0400

    Gussy up the seeds a little bit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,35 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.create(:email => 'alec.hipshear@gmail.com', :password => 'password', :password_confirmation => 'password')
-user.confirm or fail("couldn't confirm user")
-
-sherman = user.dogs.create(name: "Sherman")
-maggie = user.dogs.create(name: "Maggie")
-
-50.times do
-  sherman.posts.create(body: "Bark! " * rand(10))
+def user(email, password='password')
+  User.create!(
+    :email => email,
+    :password => password,
+    :password_confirmation => password,
+  ).tap { |u| u.confirm or fail("couldn't confirm #{email}") }
 end
 
-50.times do
-  maggie.posts.create(body: "Hmm. " * rand(10))
+alec = user('alec.hipshear@gmail.com')
+nora = user('norajaneart@gmail.com')
+janie = user('hipshear@gmail.com')
+
+dogs = [
+  alec.dogs.create(name: "Sherman"),
+  alec.dogs.create(name: "Maggie"),
+  nora.dogs.create(name: 'Egypt'),
+  janie.dogs.create(name: 'Gypsy')
+]
+
+words = [
+  'Woof! ',
+  'Hmmm. ',
+  'Meow? ',
+  'Aouuu! '
+]
+
+dogs = dogs.zip(words)
+
+100.times do
+  dog = dogs.sample
+  dog[0].posts.create(body: dog[1] + (dog[1] * rand(10)))
 end
